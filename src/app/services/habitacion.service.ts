ca53600d72362981d4bd25463718e58ef5a2c8d3
207 <207@academicos.cesde.edu.co>
2023-06-02 05:51:13 -0500

    ok

---
+++ src/app/services/habitacion.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HabitacionService {
+
+  
+  public url:string='https://pruegaapi20231.vercel.app/'
+
+  constructor(public servicioHabitacion:HttpClient) { }
+
+  buscarHabitaciones():Observable<any>{
+    let urlServicio:string=this.url+"buscarhabitaciones"
+    return this.servicioHabitacion.get(urlServicio)
+  
+  }
+
+  
+
+
+}
